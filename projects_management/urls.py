2247bcc2a33d0389ff362313f979b9732fe6d31a
yousef <yousef@ahmed.com>
2024-08-02 15:21:53 +0300

    Search and Pagination

--- projects_management/urls.py
+++ projects_management/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('', include('projects.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
